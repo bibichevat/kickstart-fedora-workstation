074849a7389b5fe5886103b1e0ae77c250f54572
bibichevat <36561119+bibichevat@users.noreply.github.com>
2019-12-24 13:21:27 +0300

    Update containers.sh

--- containers.sh
+++ containers.sh
@@ -1,4 +1,4 @@
-  
 docker run -d --restart always -p 8080:8080 -p 29418:29418 gerritcodereview/gerrit
 docker run -d --restart always -p 8090:8080 -p 50000:50000 -e JAVA_OPTS=-Djenkins.install.runSetupWizard=false docker.io/jenkins/jenkins
+sudo systemctl stop containers.service
 rm $0
